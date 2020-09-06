c8beb80b82796efbadb67dfb2a059f62451cf28c
James Noonan <warped.demon.prince@hotmail.com>
2020-09-06 14:21:33 +0800

    im a script kiddy

--- index.php
+++ index.php
@@ -29,5 +29,15 @@
     <div class="parallax"></div>
 
     <div class="parallax1"></div>
+
+    <div class="parallax2"></div>
+
+    <div class="parallax3"></div>
+
+    <div class="parallax4"></div>
+
+    <div class="parallax5"></div>
+
+    <div class="parallax6"></div>    
   </body>
 </html>

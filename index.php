9dce0ba67106cd75b567659ac744c87037e4172b
James Noonan <warped.demon.prince@hotmail.com>
2020-09-06 11:59:02 +0800

    A start
    Next up some assets

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="google-site-verification" content="txgKmeRnG--tbzmjs6dsxKpFw-x3RCL2t_jCDioBfDo" />
+    <meta name="google-site-verification" content="xkHDb2_zgTzNpDumgrN7J39GOzcAilbnu3ZnP9G_Q8A" />
+
+    <meta name="description" content="Author: J.N. Author, Length: 1 pages">
+    <meta name="keywords" content="HTML, CSS, XML, JavaScript, Php, James">
+    <meta name="author" content="James Nicholas Noonan">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="type" content="website">
+    <meta property="site_name" content="mercinna">
+
+    <link rel="stylesheet" href="Css/LandingPage.css">
+    <link rel="stylesheet" href="Css/Mobile.css">
+    <link rel="stylesheet" href="Css/Extras.css">
+    <link rel="stylesheet" href="Css/animate.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+    <script src="Js/Master.mjs"></script>
+    <script src="Js/jquery.min.js"></script>
+    <script src="Js/wow.min.js"></script>
+    <script src="Js/jquery-ui.min.js"></script>
+
+    <title> Save the kids ! </title>
+  </head>
+  <body>
+    <img src="../img/bestprojectever.jpg" style="width:100%; height:100%; display: block; margin-left: auto; margin-right: auto;"></img>
+  </body>
+</html>

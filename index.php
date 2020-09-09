c45850d7af42ea3cd1e9fd3f7861b73880860da7
James Noonan <warped.demon.prince@hotmail.com>
2020-09-09 21:18:52 +0800

    patch for mobile
    gg

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
 
     <div class="parallax img9"></div>
 
-    <video class="parallax" controls>
+    <video class="parallax video" controls>
       <source src="https://parralaxissocool.herokuapp.com/video/101_idea_globe.mp4" type="video/mp4">
       Your browser does not support the video tag.
     </video>
